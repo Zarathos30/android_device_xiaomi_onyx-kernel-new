56e3613c6379e3f4d42d48148a31c21153374206
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-11 19:42:07 +0530

    onyx-kernel: Add touch modules to vendorboot for recovery
    Change-Id: I1365a867628c8cc4dd8735294eda2a3952be00b5

--- extract-files.sh
+++ extract-files.sh
@@ -134,5 +134,11 @@ find "${extract_out}/dtbs" -type f -name "*.dtb" \
 cp -f "${extract_out}/dtbo.img" ./images/dtbo.img
 echo "Done"
 
+# Add touch modules to vendorboot for recovery
+for module in xiaomi_touch.ko nt38771_touch.ko; do
+    cp modules/vendor_dlkm/$module modules/vendor_boot/
+    echo $module >> modules/vendor_boot/modules.load.recovery
+done
+
 rm -rf $extract_out
 echo "Extracted files successfully"
